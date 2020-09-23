643e5266b22f2649f0fcccae81c4f903b4be0a45
Alejandro Mendoza <amendoza@etsy.com>
2020-09-22 20:32:23 -0500

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alejandro Mendoza on 22/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
