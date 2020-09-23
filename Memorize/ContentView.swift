ced3ac53e97e1ba1e832ec78901cc164955eb985
Alejandro Mendoza <amendoza@etsy.com>
2020-09-22 21:24:50 -0500

    Code for lecture 1: Course Logistics and Introduction to SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            ForEach(0..<4) {index in
+                CardView(isFaceUp: index % 2 == 0)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 3)
+                Text("🦙")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+            
+        }
     }
 }
 
