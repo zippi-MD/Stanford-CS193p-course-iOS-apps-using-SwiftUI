092c211b870af72fc01502ed68a79c0abb4ca3b6
Alejandro Mendoza <amendoza@etsy.com>
2020-09-28 20:07:15 -0500

    Code for Lecture 6: Animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,32 +11,71 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { (card) in
-            CardView(card: card).onTapGesture {
-                viewModel.choose(card: card)
+        VStack {
+            Grid(viewModel.cards) { (card) in
+                CardView(card: card).onTapGesture {
+                    withAnimation(.linear(duration: 0.75)) {
+                        viewModel.choose(card: card)
+                    }
+                }
+                .padding(5)
             }
-            .padding(5)
+            .foregroundColor(.orange)
+            .padding()
+            
+            Button {
+                withAnimation(.easeInOut) {
+                    viewModel.resetGame()
+                }
+            } label: {
+                Text("New game")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            }
         }
-        .foregroundColor(.orange)
-        .padding()
     }
 }
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemainig
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
 //    @ViewBuilder
     var body: some View {
         GeometryReader { geometry in
             if card.isFaceUp || !card.isMatched {
                 ZStack{
-                    Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees:20))
-                        .padding(6)
-                        .opacity(0.4)
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: -animatedBonusRemaining * 360 - 90), clockwise: true)
+                                .onAppear {
+                                    startBonusTimeAnimation()
+                                }
+                        }
+                        else {
+                            Pie(startAngle: Angle(degrees: -90), endAngle: Angle.degrees(-animatedBonusRemaining*360 - 90), clockwise: true)
+                        }
+                    }
+                    .padding(6)
+                    .opacity(0.4)
+                    .transition(.identity)
+                    
                     Text(card.content)
                         .font(Font.system(size: fontSize(for: geometry.size)))
+                        .rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
+                        .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
                 }
                 .cardify(isFaceUp: card.isFaceUp)
+                .transition(AnyTransition.scale)
             }
         }
     }
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -27,6 +27,8 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
+        
+        cards.shuffle()
     }
     
     mutating
@@ -46,9 +48,64 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                }
+                else {
+                    stopsUsingBonusTime()
+                }
+            }
+        }
+        var isMatched: Bool = false {
+            didSet {
+                stopsUsingBonusTime()
+            }
+        }
         var content: CardContent
         var id: Int
+        
+        // MARK: - Bonus Time
+        var bonusTimeLimit: TimeInterval = 6
+        
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            }
+            else {
+                return pastFaceUpTime
+            }
+        }
+        
+        var lastFaceUpDate: Date?
+        var pastFaceUpTime: TimeInterval = 0
+        
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        
+        var bonusRemainig: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        
+        var hasEarnedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        private mutating func stopsUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
     }
 }
